b6a56c75349c10d47085bed567e00e2fb89b664f
Jaewook Lee <wookboy00@naver.com>
2025-02-09 23:59:06 +0900

    Feat: add errors

--- packages/core/error.ts
+++ packages/core/error.ts
@@ -1,6 +1,22 @@
+import {Propagation} from "./propagation";
+
 export class IllegalTransactionStateException extends Error {
     constructor(propagation: string) {
         super(`Illegal transaction state: ${propagation}`);
         this.name = 'IllegalTransactionStateException';
+    }
+}
+
+export class UnsupportedTransactionPropagationException extends Error {
+    constructor() {
+        super(`Unsupported propagation. Supported propagations: ${Object.values(Propagation).join(", ")}`);
+        this.name = 'UnsupportedTransactionPropagationException';
+    }
+}
+
+export class DatasourceSetupError extends Error {
+    constructor() {
+        super(`Datasource is not set up. `);
+        this.name = 'TransactionDatasourceSetupError';
     }
 }
