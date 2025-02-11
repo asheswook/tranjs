d2bcf16381ba52582df8900b4e82ec113870bf49
Jaewook Lee <wookboy00@naver.com>
2025-02-11 13:26:25 +0900

    Fix: mysql2 execute is generic

--- packages/mysql2/src/context.ts
+++ packages/mysql2/src/context.ts
@@ -4,7 +4,7 @@ import {FieldPacket, Pool, PoolConnection, QueryResult} from "mysql2/promise";
 export class MySQLConnection implements TransactionContext<[QueryResult, FieldPacket[]]> {
     constructor(public connection: PoolConnection) {}
 
-    async execute(query: string, params?: any[]): Promise<[QueryResult, FieldPacket[]]> {
-        return this.connection.execute(query, params)
+    async execute<T extends QueryResult>(query: string, params?: any[]): Promise<[T, FieldPacket[]]> {
+        return this.connection.execute<T>(query, params)
     }
 }
