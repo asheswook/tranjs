6add3f8466f5c27991d6b344ff8103919cf6d618
Jaewook Lee <wookboy00@naver.com>
2025-04-22 23:27:59 +0900

    Chore: transaction config draft

---
+++ packages/core/src/config.ts
@@ -0,0 +1,23 @@
+import {Propagation} from "./propagation";
+
+export interface TransactionConfig {
+    // /**
+    //  * The name of the transaction. just for logging purposes.
+    //  */
+    // name?: string;
+
+    // /**
+    //  * The isolation level of the transaction.
+    //  */
+    // isolationLevel?: string;
+
+    /**
+     * The timeout of the transaction in milliseconds.
+     */
+    timeout?: number;
+
+    /**
+     * The propagation behavior of the transaction.
+     */
+    propagation?: Propagation;
+}
